f4490039e14e2365620d35a7e79e796b4636e5f8
szee45 <shah.nawaz@sstsinc.com>
2018-07-26 11:16:54 +0530

    Android change

--- android1.sh
+++ android1.sh
@@ -7,14 +7,14 @@ adb install -r pCloudy_Appium_Demo.apk
 package=`aapt dump badging *.apk | grep package | awk '{print $2}' | sed s/name=//g | sed s/\'//g`
 activity=`aapt dump badging *.apk | grep launchable-activity: | awk '{print $2}' | sed s/name=//g | sed s/\'//g`
 adb shell am start -n $package/$activity
-echo "Opening app"
+echo "Opening app.."
 sleep 1
-echo "Accepting terms"
+echo "Accepting terms.."
 adb shell input keyevent 66
 #adb shell input keyevent 66
 #adb shell input keyevent 66
 adb shell input keyevent 66
-echo "Entering credentials"
+echo "Entering credentials.."
 read -p "Enter the club number: " usr
 read -p "Enter the password in numeric form: " pass
 adb shell input text $usr
@@ -23,7 +23,7 @@ adb shell input keyevent 61
 adb shell input text $pass
 adb shell input keyevent 61
 adb shell input keyevent 66
-echo "Selecting Destination"
+echo "Selecting Destination.."
 adb shell input keyevent 66
 adb shell input keyevent 20
 adb shell input keyevent 20
@@ -33,12 +33,12 @@ adb shell input keyevent 66
 adb shell input swipe 100 1200 100 400 100
 adb shell input tap 257 332
 adb shell input keyevent 66
-echo "Seleciting Seats"
+echo "Seleciting Seats.."
 adb shell input keyevent 20
 adb shell input keyevent 20
 adb shell input keyevent 20
 adb shell input keyevent 66
-echo "Searching Flights"
+echo "Searching Flights.."
 adb shell input keyevent 20
 adb shell input keyevent 20
 adb shell input keyevent 66
@@ -47,13 +47,13 @@ adb shell input keyevent 20
 adb shell input keyevent 66
 sleep 2
 adb shell screencap -p /sdcard/mytrip.png
-echo "Saving your travel "
-tput setaf 1; echo "The trip information is saved in your gallery"
+echo "Saving your travel.. "
+tput setaf 1; echo "The trip information is saved in your gallery."
 adb pull /sdcard/mytrip.png
 sleep 1
 adb shell input keyevent 4
 echo " "
-tput setaf 2; echo "Following is the device information"
+tput setaf 2; echo "Following is the device information."
 adb shell getprop | grep "model\|version.sdk\|manufacturer\|hardware\|platform\|revision\|serialno\|product.name\|brand" $info
 adb shell cat proc/cpuinfo
 echo " "
